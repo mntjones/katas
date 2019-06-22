bef463daf8c7b3d1a4b262725abb6ea7019c8a5c
Monica <mntamaru@gmail.com>
2019-06-21 18:55:55 -0700

    add skeleton for singly linked list

---
+++ kata_21/kata_21.rb
@@ -0,0 +1,33 @@
+
+# Implementation of Kata 21
+# 1. A singly linked list (each node has a reference to the next node).
+# 2. A doubly linked list (each node has a reference to both the next and previous nodes).
+# 3. Some other implementation of your choosing, except that it should use no references 
+# 		(pointers) to collect nodes together in the list.
+
+class singlyLinkedList
+
+	def initialize(str)
+		@str = str
+
+		@ref_node = 0
+		@all = []
+
+	end
+
+	# searches for str and returns node
+	def find_node(str)
+
+	end
+
+	# simple add to end
+	def add_node
+
+	end
+
+	# will need to rename nodes that come after deleted node
+	def delete_node(node) 
+
+	end
+
+end
